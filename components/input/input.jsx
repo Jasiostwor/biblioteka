1e959d6f8ac5f815c5df5180c2b9301c9d1d68a7
bepti <filipkaczmarek@vp.pl>
2023-05-25 07:56:21 +0200

    onChange added

--- components/input/input.jsx
+++ components/input/input.jsx
@@ -1,13 +1,6 @@
 import styles from "./input.module.css";
 
 export default function Input(props) {
-  const { placeholder, name, type } = props;
-  return (
-    <input
-      type={type}
-      placeholder={placeholder}
-      name={name}
-      className={styles.input}
-    />
-  );
+    const { placeholder, name, type, onChange } = props;
+    return <input type={type} placeholder={placeholder} name={name} className={styles.input} onChange={onChange} />;
 }
--- components/logoutButton/logoutButton.jsx
+++ components/logoutButton/logoutButton.jsx
@@ -2,28 +2,29 @@
 import { BiUserCircle } from "react-icons/bi";
 import { useState } from "react";
 import styles from "./logoutButton.module.css";
+import Link from "next/link";
 
 export default function LogoutButton() {
-  const [modal, setModal] = useState(false);
+    const [modal, setModal] = useState(false);
 
-  const toggleModal = () => {
-    setModal(!modal);
-  };
+    const toggleModal = () => {
+        setModal(!modal);
+    };
 
-  return (
-    <>
-      <button
-        className={`${styles.iconBtn} ${styles.iconUserBtn}`}
-        onClick={toggleModal}>
-        <BiUserCircle className={`${styles.icon} ${styles.iconUser}`} />
-      </button>
+    return (
+        <>
+            <button className={`${styles.iconBtn} ${styles.iconUserBtn}`} onClick={toggleModal}>
+                <BiUserCircle className={`${styles.icon} ${styles.iconUser}`} />
+            </button>
 
-      {modal && (
-        <div className={styles.modal}>
-          <h3>Email użytkownika</h3>
-          <button className={styles.logoutBtn}>Wyloguj się</button>
-        </div>
-      )}
-    </>
-  );
+            {modal && (
+                <div className={styles.modal}>
+                    <h3>Email użytkownika</h3>
+                    <Link href="/login">
+                        <button className={styles.logoutBtn}>Wyloguj się</button>
+                    </Link>
+                </div>
+            )}
+        </>
+    );
 }
